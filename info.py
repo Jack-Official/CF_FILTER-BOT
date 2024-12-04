c3b70f624ea58850bffd0a61bfcd56379b5eb31b
Kl-FaKe-TG <suneeshboy00@gmail.com>
2024-12-05 03:24:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,6 +31,7 @@ PICS = (environ.get('PICS' ,'https://envs.sh/AOQ.jpg')).split()
 NOR_IMG = environ.get('NOR_IMG', "https://envs.sh/PvY.jpg")
 SPELL_IMG = environ.get('SPELL_IMG', "https://envs.sh/AOI.jpg")
 MOVIE = environ.get('MOVIE', "https://envs.sh/AOI.jpg")
+FORCE_IMG = environ.get('FORCE_IMG', "........")
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
