1976f3b7b5b090f60a9f532dcd9013af51318320
Kl-FaKe-TG <suneeshboy00@gmail.com>
2024-12-10 02:22:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -335,7 +335,7 @@ async def filter_languages_cb_handler(client: Client, query: CallbackQuery):
         )
 
     search = f"{search1} {lang}"     
-    files, offset, total = await get_search_results(search, max_results=8)
+    files, offset, total = await get_search_results(search, offset=offset, filter=True)
     files = [file for file in files if re.search(lang, file.file_name, re.IGNORECASE)]
     if not files:
         await query.answer("🚫 𝗡𝗼 𝗙𝗶𝗹𝗲 𝗪𝗲𝗿𝗲 𝗙𝗼𝘂𝗻𝗱 🚫", show_alert=1)
